386292274052e41ac7662330a7e78dfb429e53df
smuffels <bruncel1@students.zhaw.ch>
2026-02-23 16:55:44 +0100

    add 1480

---
+++ 1480_running_sum_of_1d_array/1480_running_sum_of_1d_array_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+class  aaah{
+    public static void Main()
+    {
+        int[] testArray = {1,2,3,4};
+        Solution solution = new Solution();
+        int[] result = solution.RunningSum(testArray);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+    
+    
+}
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        int[] sumArray = new int[nums.Length];
+        int sum = 0;
+
+        if (nums.Length < 1 || nums.Length >1000){
+            return sumArray;
+        }
+        for(int i = 0; i< nums.Length; i++){
+            if(nums[i]>-1000000 && nums[i]<1000000){
+                sum = sum + nums[i];
+                sumArray[i] = sum;
+            }
+            
+        }
+        return sumArray;
+    }
+}
+
+
