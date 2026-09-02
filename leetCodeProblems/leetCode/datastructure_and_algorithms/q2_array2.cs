01cdc69aaeb8fbd8fc08a0e320f7df577c994e23
smuffels <bruncel1@students.zhaw.ch>
2026-09-02 11:34:56 +0200

    solve array part of quest

--- leetCodeProblems/leetCode/datastructure_and_algorithms/q2_array2.cs
+++ leetCodeProblems/leetCode/datastructure_and_algorithms/q2_array2.cs
@@ -2,5 +2,22 @@ namespace leetCode.datastructure_and_algorithms;
 
 public class q2_array2
 {
-    
+    public int[] SmallerNumbersThanCurrent(int[] nums)
+    {
+        int counter;
+        var solution = new int[nums.Length];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            counter = 0;
+            foreach (var number in nums)
+            {
+                if (number < nums[i] && number != nums[i]) counter++;
+
+                solution[i] = counter;
+            }
+        }
+
+        return solution;
+    }
 }
--- leetCodeProblems/leetCode/datastructure_and_algorithms/q3_array2.cs
+++ leetCodeProblems/leetCode/datastructure_and_algorithms/q3_array2.cs
@@ -2,5 +2,16 @@ namespace leetCode.datastructure_and_algorithms;
 
 public class q3_array2
 {
-    
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        IList<int> result = new List<int>();
+        var min = 1;
+        var max = nums.Length;
+
+        for (var i = min; i <= max; i++)
+            if (!nums.Contains(i))
+                result.Add(i);
+
+        return result;
+    }
 }
